4a30a1e36d7eef4b2044d25c23630ccdd5854517
unknown <xabda001@gmail.com>
2022-10-06 18:47:56 +0600

    3

--- js.js
+++ js.js
@@ -1,4 +1,3 @@
-let Users = []
 class User {
     constructor(email, password, name, surname, age) {
         this.email = email;
@@ -8,21 +7,43 @@ class User {
         this.age = age;
     }
 }
+
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+}
+
+function checkData(newUser) {
+    if (!validateEmail(newUser.email)) {
+        alert("Incorrect email!")
+        return false;
+    } else if (newUser.Name.length < 1 || newUser.Surname.length < 1
+    || newUser.password.length < 1 || newUser.age.length < 1) {
+        alert('Complete all data!')
+        return false;
+    } else {
+        return true;
+    }   
+}
+
+let Users = []
 function fnRegistraton() {
     let email = document.querySelector('.email').value
     let password = document.querySelector('.password').value
     let name = document.querySelector('.name').value
     let surname = document.querySelector('.surname').value
     let age = document.querySelector('.age').value
-
-    if (email && password && name && surname && age) {
-        let newUser = new User(
-            email,
-            password,
-            name,
-            surname,
-            age
-        )
+    let newUser = new User(
+        email,
+        password,
+        name,
+        surname,
+        age
+    )
+    if (checkData(newUser)) {
         Users.push(newUser)
         localStorage.setItem('Users', JSON.stringify(Users))
     }
@@ -33,13 +54,15 @@ function fnLogin() {
     let checkUserInArr = JSON.parse(localStorage.getItem('Users'))
     let email = document.querySelector('.loginEmail').value
     let password = document.querySelector('.loginPassword').value
-
-    for (let i = 0; i <= checkUserInArr.length - 1; i++) {
-        if (email === checkUserInArr[i].email && password === checkUserInArr[i].password) {
-            document.location.href = './Home_page.html'
-            localStorage.setItem('currentUser', JSON.stringify(checkUserInArr[i]))
+    if(localStorage.getItem('Users')){
+        for (let i = 0; i <= checkUserInArr.length - 1; i++) {
+            if (email === checkUserInArr[i].email && password === checkUserInArr[i].password) {
+                window.location.href = './Home_page.html'
+                localStorage.setItem('currentUser', JSON.stringify(checkUserInArr[i]))
+            }
         }
     }
+    
 }
 
 if(localStorage.getItem('currentUser')){
@@ -47,8 +70,31 @@ if(localStorage.getItem('currentUser')){
     let homePageSurname = document.querySelector('.homePageSurname')
     let homePageAge = document.querySelector('.homePageAge')
     let homePageArr = JSON.parse(localStorage.getItem('currentUser'))
-    homePageName.textContent = homePageName.textContent + ': ' + homePageArr.Name
+    homePageName.textContent = `Name: ${homePageArr.Name}`
     homePageSurname.textContent = homePageSurname.textContent + ': ' + homePageArr.Surname
     homePageAge.textContent = homePageAge.textContent + ': ' + homePageArr.age
 }
+
+function logOut(){
+    window.location.href = './index.html'
+    localStorage.removeItem('currentUser')
+}
+
+function deleteAcc(){
+    let toDelete = JSON.parse(localStorage.getItem('Users'))
+    let toCheck = JSON.parse(localStorage.getItem('currentUser'))
+    for(let i = 0; i <= toDelete.length - 1; i++){
+        if(toDelete[i].email === toCheck.email && toDelete[i].password === toCheck.password){
+            toDelete.splice(i, 1)
+            localStorage.setItem('Users', JSON.stringify(toDelete))
+            localStorage.removeItem('currentUser')
+            window.location.href = './registration.html'
+        }
+    }
+}
+
+
+
+
+
 
