d9ee00e4958f42fd8d33d445f9bb4689ab0de719
Абдыгалиев Аслан Абзалович <AbdygalievA@lcoast.itstep>
2022-10-06 21:53:38 +0600

    4

--- js.js
+++ js.js
@@ -29,7 +29,6 @@ function checkData(newUser) {
     }   
 }
 
-let Users = []
 function fnRegistraton() {
     let email = document.querySelector('.email').value
     let password = document.querySelector('.password').value
@@ -43,9 +42,17 @@ function fnRegistraton() {
         surname,
         age
     )
-    if (checkData(newUser)) {
+    let Users = []
+    if(localStorage.getItem('Users')){
+        Users = JSON.parse(localStorage.getItem('Users')) 
+    }
+    let checkSimilarEmail = Users.filter(user => user.email == newUser.email)
+    if (checkData(newUser) && checkSimilarEmail.length < 1) {
         Users.push(newUser)
         localStorage.setItem('Users', JSON.stringify(Users))
+        window.location.href = './index.html'
+    }else if(checkSimilarEmail.length > 0){
+        alert('You are already registered')
     }
 }
 
@@ -54,26 +61,35 @@ function fnLogin() {
     let checkUserInArr = JSON.parse(localStorage.getItem('Users'))
     let email = document.querySelector('.loginEmail').value
     let password = document.querySelector('.loginPassword').value
+    let checkSimilarEmail = JSON.parse(localStorage.getItem('Users')).filter(user => user.email == email)
     if(localStorage.getItem('Users')){
         for (let i = 0; i <= checkUserInArr.length - 1; i++) {
             if (email === checkUserInArr[i].email && password === checkUserInArr[i].password) {
                 window.location.href = './Home_page.html'
-                localStorage.setItem('currentUser', JSON.stringify(checkUserInArr[i]))
+                localStorage.setItem('currentUser', JSON.stringify({...checkUserInArr[i], isRemember: document.querySelector('#checkbox').checked}))
+                let homePageName = document.querySelector('.homePageName')
+                let homePageSurname = document.querySelector('.homePageSurname')
+                let homePageAge = document.querySelector('.homePageAge')
+                let homePageArr = JSON.parse(localStorage.getItem('currentUser'))
+                homePageName.textContent = homePageName.textContent + ': ' + homePageArr.Name
+                homePageSurname.textContent = homePageSurname.textContent + ': ' + homePageArr.Surname
+                homePageAge.textContent = homePageAge.textContent + ': ' + homePageArr.age
+                break
+            }else if(email === checkUserInArr[i].email && password !== checkUserInArr[i].password){
+                alert('Incorrect password')
+                break
+            }else if(checkSimilarEmail < 1){
+                alert('Incorrect email or You are not registered')
+                break
             }
         }
     }
-    
+    if(localStorage.getItem('currentUser') == true){
+        
+    }
 }
 
-if(localStorage.getItem('currentUser')){
-    let homePageName = document.querySelector('.homePageName')
-    let homePageSurname = document.querySelector('.homePageSurname')
-    let homePageAge = document.querySelector('.homePageAge')
-    let homePageArr = JSON.parse(localStorage.getItem('currentUser'))
-    homePageName.textContent = `Name: ${homePageArr.Name}`
-    homePageSurname.textContent = homePageSurname.textContent + ': ' + homePageArr.Surname
-    homePageAge.textContent = homePageAge.textContent + ': ' + homePageArr.age
-}
+
 
 function logOut(){
     window.location.href = './index.html'
@@ -92,9 +108,3 @@ function deleteAcc(){
         }
     }
 }
-
-
-
-
-
-
